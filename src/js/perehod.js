22cf272216c92a2726cb74b25afeb39dcf92b8e9
maksim25y <131711956+maksim25y@users.noreply.github.com>
2023-04-30 16:22:48 +0300

    Update and rename perehod.js to script.js

--- src/js/perehod.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  $("a.scrollto").click(function () {
-   let elementClick = $(this).attr("href")
-   let destination = $(elementClick).offset().top;
-   $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 3000);
-   return false;
-  });
-});
